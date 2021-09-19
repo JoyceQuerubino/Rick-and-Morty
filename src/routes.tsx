507e3953e468ad69dbc3dbedff5f3c4049363c9b
JoyceQuerubino <joycequerubino5@gmail.com>
2021-09-19 02:36:49 -0300

    Removendo cabeçalho

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,7 +7,11 @@ const Stack = createNativeStackNavigator();
 
 const MainRoute = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
