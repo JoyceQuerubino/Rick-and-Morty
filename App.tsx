9cd4710cf8404854c03320fcc81944628eebf74a
JoyceQuerubino <joycequerubino5@gmail.com>
2021-09-19 02:37:09 -0300

    Adicionando fontes e tema

--- App.tsx
+++ App.tsx
@@ -1,18 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/theme';
 import {Provider} from 'react-redux';
+
+import {
+  useFonts, 
+  Roboto_700Bold, 
+  Roboto_400Regular, 
+} from '@expo-google-fonts/roboto';
 
 import MainRoute from './src/routes';
 import { Store } from './src/redux/store';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold, 
+    Roboto_400Regular, 
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
-    <Provider store={Store}>
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <MainRoute />
-    </NavigationContainer>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={Store}>
+      <NavigationContainer>
+        <StatusBar style="dark"/>
+        <MainRoute />
+      </NavigationContainer>
+      </Provider>
+    </ThemeProvider>
   );
 }
