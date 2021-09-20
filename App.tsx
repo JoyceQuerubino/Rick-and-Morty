161f028255e2dc747ed588bc1291d30fc76e6325
JoyceQuerubino <joycequerubino5@gmail.com>
2021-09-20 10:48:02 -0300

    Instalando axios

--- App.tsx
+++ App.tsx
@@ -3,13 +3,11 @@ import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components';
-import { Provider } from 'react-redux';
 
-import store from './src/redux/store'; 
 import theme from './src/global/theme';
 import {
   useFonts, 
-  Roboto_700Bold, 
+  Roboto_700Bold,
   Roboto_400Regular, 
 } from '@expo-google-fonts/roboto';
 
@@ -22,18 +20,16 @@ export default function App() {
     Roboto_400Regular, 
   });
 
-  // if(!fontsLoaded){
-  //   return <AppLoading />
-  // }
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
 
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <StatusBar style="dark"/>
-          <MainRoute />
-        </NavigationContainer>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar style="dark"/>
+        <MainRoute />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
