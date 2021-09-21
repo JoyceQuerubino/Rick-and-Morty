e03ed601b362b9528804ed8f981edd68b81b6ad8
JoyceQuerubino <joycequerubino5@gmail.com>
2021-09-21 02:08:18 -0300

    Add novas cores

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -2,8 +2,10 @@ export default {
     colors: {
         primary: '#63D477', 
         secondary: '#403F3F',
+        tertiary: '#313131', 
         background: '#1D1C1C',
         text: '#FFFFFF', 
+        dark_text: '#2E2E2E', 
     }, 
 
     fonts: {
