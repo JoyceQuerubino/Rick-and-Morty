0acbff4637affe7221fef69f8059b7992db3b1a6
JoyceQuerubino <joycequerubino5@gmail.com>
2021-09-23 03:32:09 -0300

    Corrigir estilizacao

--- src/views/Favorites/styles.ts
+++ src/views/Favorites/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { RFValue } from 'react-native-responsive-fontsize'; 
+import { RFValue, RFPercentage } from 'react-native-responsive-fontsize'; 
 import { FlatList } from 'react-native';
 import { CharactereCardProps } from '.'; //importada da propria pasta
 
@@ -12,12 +12,16 @@ export const Title = styled.Text`
     color: ${({ theme }) => theme.colors.text};
     font-family: ${({ theme }) => theme.fonts.bold};
     font-size: ${RFValue(32)}px;
-    margin-bottom: 24px;
+    margin-bottom: 16px;
 `;
 
 export const Content = styled.View`
     padding: 0px 24px;
+    height: 100%;
 `;
 
 export const CharacteresList = styled(
-    FlatList as new () => FlatList<CharactereCardProps>)``
+FlatList as new () => FlatList<CharactereCardProps>)`
+    margin-bottom: 16px;
+    max-height: ${RFPercentage(90)}px;
+`; 
